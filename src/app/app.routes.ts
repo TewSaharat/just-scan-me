ca13fdfdfa97e4d719da99e8e13a00fd0e8df601
TewSaharat <tewsaharat17@gmail.com>
2025-03-05 23:17:55 +0700

    updetes package.jsion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,13 @@ import { NotifyComponent } from './notify/notify.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { QrCodePageComponent } from './qr-code-page/qr-code-page.component';
 import { provideRouter, withHashLocation } from '@angular/router';
-import { AppComponent } from './app.component';
-import { UploadKMLComponent } from './upload-kml/upload-kml.component';
 
 
 export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'notify', component: NotifyComponent },
-    { path: 'QrCode', component: QrCodePageComponent },
-    {path:'upload' ,component:UploadKMLComponent}
-
+    { path: 'QrCode', component: QrCodePageComponent }
   ];
   
   export const APP_ROUTER = provideRouter(routes, withHashLocation());
