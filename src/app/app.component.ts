69a64495e182627c5fc6a4f5e692b50ff6a26e51
TewSaharat <tewsaharat17@gmail.com>
2025-03-06 01:36:49 +0700

    Fixed router navigation and baseHref issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { RouterModule, RouterOutlet, } from '@angular/router';
-import { HomePageComponent } from "./home-page/home-page.component";
-import { BodyComponent } from "./body/body.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [ RouterModule, CommonModule, HomePageComponent],
+    imports: [ RouterModule, CommonModule,RouterOutlet ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
