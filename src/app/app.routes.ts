5f1cbbc42cc2aea19a148dca0d6e4e4e70e974a7
TewSaharat <tewsaharat17@gmail.com>
2025-03-06 01:30:39 +0700

    Fixed router navigation and baseHref issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { UploadKMLComponent } from './upload-kml/upload-kml.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: '', redirectTo: '/', pathMatch: 'full' }, // ✅ Redirect ไปหน้า Home
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // ✅ Redirect ไปหน้า Home
   { path: '**', redirectTo: '/' }, // ✅ ถ้าไม่พบเส้นทาง ให้กลับไปที่ Home
     { path: 'notify', component: NotifyComponent },
     { path: 'QrCode', component: QrCodePageComponent },
