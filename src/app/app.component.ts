3451c0dadccc1092481665fd763aae93b5d3deaa
TewSaharat <tewsaharat17@gmail.com>
2025-03-05 21:27:28 +0700

    up dete login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { RouterModule, RouterOutlet, } from '@angular/router';
+import { HomePageComponent } from "./home-page/home-page.component";
+import { BodyComponent } from "./body/body.component";
 
 @Component({
     selector: 'app-root',
