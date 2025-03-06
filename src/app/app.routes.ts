28c4d5108755d07092d569e8331af4ff3290dd8e
TewSaharat <tewsaharat17@gmail.com>
2025-03-07 04:30:08 +0700

    make upload page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from '@angular/router';
 import { NotifyComponent } from './notify/notify.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { QrCodePageComponent } from './qr-code-page/qr-code-page.component';
+import { KmlUploadComponent } from './kml-upload/kml-upload.component';
 
 
 export const routes: Routes = [
     {path:'home',component:HomePageComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path: 'notify',component:NotifyComponent},
-    {path: 'QrCode',component:QrCodePageComponent}
+    {path: 'QrCode',component:QrCodePageComponent},
+    {path:'upload',component:KmlUploadComponent}
 ];
 
